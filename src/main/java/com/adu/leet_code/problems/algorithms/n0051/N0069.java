b01f9c40ab2b0634b89d1f967f9db884a8db9510
yunjie.du <yunjie.du@qunar.com>
2017-08-08 19:45:50 +0800

    ADD:69. Sqrt(x)

---
+++ src/main/java/com/adu/leet_code/problems/algorithms/n0051/N0069.java
@@ -0,0 +1,37 @@
+package com.adu.leet_code.problems.algorithms.n0051;
+
+/**
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x.
+ * 
+ * @author yunjie.du
+ * @date 2017/8/8 18:44
+ * @see <a href="https://leetcode.com/problems/sqrtx/description/">69. Sqrt(x)</a>
+ */
+public class N0069 {
+    /**
+     * 二分查找。
+     * 
+     * @param x
+     * @return
+     */
+    public int mySqrt(int x) {
+        int left = 0, right = x / 2 + 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            long square = ((long) mid) * mid;
+            if (square == x) {
+                return mid;
+            }
+            if (square > x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return x >= right * right ? right : left;
+    }
+
+}
---
+++ src/test/java/com/adu/leet_code/problems/algorithms/n0051/N0069Test.java
@@ -0,0 +1,19 @@
+package com.adu.leet_code.problems.algorithms.n0051;
+
+import org.junit.Test;
+
+import com.adu.leet_code.BaseTest;
+
+/**
+ * @author yunjie.du
+ * @date 2017/8/8 18:49
+ */
+public class N0069Test extends BaseTest {
+    private N0069 n0069 = new N0069();
+
+    @Test
+    public void mySqrt() {
+        int res = n0069.mySqrt(77);
+        logRes(res);
+    }
+}
